b72846809523e514fbd3d1903b00120849df199c
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-05 00:41:21 +0700

    Add files via upload

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ini halaman about</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<a href="/">home</a>
+<a href="/about">about</a>
+    <h1>halaman about</h1>
+    <h3>sekolah: {{ $sekolah }}</h3>
+    <img src="img\animals_hero_sugarglider.jpg" alt="sugar glider" width="500">
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halaman home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <h1>halaman home</h1>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
