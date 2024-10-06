d178c0b72e7ea84c03ac9893556bbc8cf776c015
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-07 00:47:45 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,19 @@
 <?php
 
+use App\Models\post;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Builder\Function_;
 
-Route::get('/', function () {
-    return view('home', ['homes' => [
-        [
-            'id' => 1,
-            'slug' => 'artikel-1',
-            'title' => 'artikel 1',
-            'author' => 'ayam goreng',
-            'body' => '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ante ex, iaculis in bibendum nec, molestie vel odio. Duis pretium neque quis massa auctor, ac convallis mauris porttitor. Quisque felis velit, tempor vel ipsum non, elementum lobortis justo. Maecenas ac gravida dolor. Maecenas consequat eros sit amet nulla blandit semper. Mauris tristique at augue vel tempor. Vivamus hendrerit nunc lorem, 
-          vitae placerat est consequat et. Donec at dapibus metus, et pharetra purus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ante ex, iaculis in bibendum nec, molestie vel odio. Duis pretium neque quis massa auctor, ac convallis mauris porttitor. Quisque felis velit, tempor vel ipsum non, elementum lobortis justo. Maecenas ac gravida dolor. Maecenas consequat eros sit amet nulla blandit semper. 
-          Mauris tristique at augue vel tempor. Vivamus hendrerit nunc lorem, vitae placerat est consequat et. Donec at dapibus metus, et pharetra purus.'
-        ],
 
-        [
-            'id' => 2,
-            'slug' => 'artikel-2',
-            'title' => 'artikel 2',
-            'author' => 'ayam goreng',
-            'body' => '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ante ex, iaculis in bibendum nec, molestie vel odio. Duis pretium neque quis massa auctor, ac convallis mauris porttitor. Quisque felis velit, tempor vel ipsum non, elementum lobortis justo. Maecenas ac gravida dolor. Maecenas consequat eros sit amet nulla blandit semper. Mauris tristique at augue vel tempor. Vivamus hendrerit nunc lorem, 
-          vitae placerat est consequat et. Donec at dapibus metus, et pharetra purus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ante ex, iaculis in bibendum nec, molestie vel odio. Duis pretium neque quis massa auctor, ac convallis mauris porttitor. Quisque felis velit, tempor vel ipsum non, elementum lobortis justo. Maecenas ac gravida dolor. Maecenas consequat eros sit amet nulla blandit semper. 
-          Mauris tristique at augue vel tempor. Vivamus hendrerit nunc lorem, vitae placerat est consequat et. Donec at dapibus metus, et pharetra purus.'
-        ]
-    ]]);
+
+Route::get('/', function () {
+    return view('home', ['homes' => post::all()]);
 
 
 });
 
 Route::get('/{slug}', Function($slug) {
-    
+    $post = Post::find($slug);
 }); 
 
 Route::get('/about', function(){
