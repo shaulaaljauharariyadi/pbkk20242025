52ed666f1b905f859a23d91a21c4647df1915a72
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-06 14:51:43 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>halaman home</title>
-    <link rel="stylesheet" href="c<!DOCTYPE html>
 <html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
@@ -124,19 +118,25 @@
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <!-- Your content -->
+       @foreach ($homes as $post)
+         
+       <article class="py-8 max-w-screen-md border-b border-gray-500">
+        <a href="/{{$post['slug']}}" class="hover:underline">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{$post['title']}}</h2>
+        </a>
+        <div class="text-base text-gray-500">
+          <a href="#">{{$post['title']}}</a> | 1 januari 2024
+        </div>
+        <p class="my-4 font-light">{{ Str::limit($post['body'], 50) }}</p>
+          <a href="/{{$post['slug']}}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+       </article>
+       @endforeach 
+
+
     </div>
   </main>
 </div>
 
 
-</body>
-</html>ss/style.css">
-</head>
-<body>
-    <a href="/">home</a>
-    <a href="/about">about</a>
-    <h1>halaman home</h1>
-
-    <script src="js/script.js"></script>
 </body>
 </html>
