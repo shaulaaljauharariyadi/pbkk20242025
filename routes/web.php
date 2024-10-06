c85a7b92fc1a9052fb0e1a29741b2575efa93f00
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-06 14:51:03 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Builder\Function_;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['homes' => [
+        [
+            'id' => 1,
+            'slug' => 'artikel-1',
+            'title' => 'artikel 1',
+            'author' => 'ayam goreng',
+            'body' => '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ante ex, iaculis in bibendum nec, molestie vel odio. Duis pretium neque quis massa auctor, ac convallis mauris porttitor. Quisque felis velit, tempor vel ipsum non, elementum lobortis justo. Maecenas ac gravida dolor. Maecenas consequat eros sit amet nulla blandit semper. Mauris tristique at augue vel tempor. Vivamus hendrerit nunc lorem, 
+          vitae placerat est consequat et. Donec at dapibus metus, et pharetra purus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ante ex, iaculis in bibendum nec, molestie vel odio. Duis pretium neque quis massa auctor, ac convallis mauris porttitor. Quisque felis velit, tempor vel ipsum non, elementum lobortis justo. Maecenas ac gravida dolor. Maecenas consequat eros sit amet nulla blandit semper. 
+          Mauris tristique at augue vel tempor. Vivamus hendrerit nunc lorem, vitae placerat est consequat et. Donec at dapibus metus, et pharetra purus.'
+        ],
+
+        [
+            'id' => 2,
+            'slug' => 'artikel-2',
+            'title' => 'artikel 2',
+            'author' => 'ayam goreng',
+            'body' => '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ante ex, iaculis in bibendum nec, molestie vel odio. Duis pretium neque quis massa auctor, ac convallis mauris porttitor. Quisque felis velit, tempor vel ipsum non, elementum lobortis justo. Maecenas ac gravida dolor. Maecenas consequat eros sit amet nulla blandit semper. Mauris tristique at augue vel tempor. Vivamus hendrerit nunc lorem, 
+          vitae placerat est consequat et. Donec at dapibus metus, et pharetra purus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ante ex, iaculis in bibendum nec, molestie vel odio. Duis pretium neque quis massa auctor, ac convallis mauris porttitor. Quisque felis velit, tempor vel ipsum non, elementum lobortis justo. Maecenas ac gravida dolor. Maecenas consequat eros sit amet nulla blandit semper. 
+          Mauris tristique at augue vel tempor. Vivamus hendrerit nunc lorem, vitae placerat est consequat et. Donec at dapibus metus, et pharetra purus.'
+        ]
+    ]]);
+
+
 });
+
+Route::get('/{slug}', Function($slug) {
+    
+}); 
 
 Route::get('/about', function(){
     return view('about', ['sekolah' => 'ayam ikan 21']);
