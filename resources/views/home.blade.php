0459d76fb049ff30d6a22ac81dfd83a35175bbe7
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-07 12:59:37 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -125,7 +125,7 @@
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{$post['title']}}</h2>
         </a>
         <div class="text-base text-gray-500">
-          <a href="#">{{$post['title']}}</a> | 1 januari 2024
+          <a href="#">{{$post['title']}}</a> | {{$post->created_at->format('j F Y')}}
         </div>
         <p class="my-4 font-light">{{ Str::limit($post['body'], 50) }}</p>
           <a href="/{{$post['slug']}}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
