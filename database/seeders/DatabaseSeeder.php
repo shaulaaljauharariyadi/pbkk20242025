9e473423f8c2a152991703d2407e3b7dc624d0f4
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-08 13:54:56 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+       // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       // User::factory()->create([
+       //     'name' => 'Test User',
+       //     'email' => 'test@example.com',
+       // ]);
+
+
+       // Post::create([
+       // 'title'=>'artikel 1',
+      //'author_id'=>'1',
+      //  'slug'=>'artikel-1',
+      //  'body'=>'The foreignId method creates an UNSIGNED BIGINT equivalent column, while the constrained method will use conventions to determine the table and column being referenced. If your table name does not match Laravels 
+     // conventions, you may manually provide it to the constrained method. In addition, the name that should be 
+     //   assigned to the generated index may be specified as well'
+     //  ]);
+     $this->call([UserSeeder::class]);
+
+     Post::factory(100)->recycle([
+        User::all()
+     ])->create();
+
+
     }
 }
